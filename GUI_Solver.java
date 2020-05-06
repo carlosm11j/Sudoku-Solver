388eba477c2b4dd1a1ac03fc4b2c61af60873e89
Carlos Mercado <cmercado@scu.edu>
2020-05-06 15:29:38 -0400

    Add Check Button

--- GUI_Solver.java
+++ GUI_Solver.java
@@ -84,6 +84,10 @@ public class GUI_Solver extends JFrame implements ActionListener {
     JButton solveButton = new JButton("Solve");
 		south.add(solveButton);
 		solveButton.addActionListener(this);
+    //Solve Button
+    JButton checkButton = new JButton("Check");
+		south.add(checkButton);
+		checkButton.addActionListener(this);
     //Exit Button
 		JButton exitButton = new JButton("Random");
 		south.add(exitButton);
@@ -112,6 +116,7 @@ public class GUI_Solver extends JFrame implements ActionListener {
       }
     }
     else if(word.equals("Check")) {
+      getBoard(sodokuBoard);
       checkUserEntries(sodokuBoard);
     }
 	}
@@ -122,14 +127,12 @@ public class GUI_Solver extends JFrame implements ActionListener {
     //Check row: O(1)
     for(int i=0; i<board.length; i++) {
       if(board[row][i] == entry) {
-        //System.out.println("ROW CHECK for entry: row = " + row + ", col = " + i + ", entry = " + board[row][col]);
         return false;
       }
     }
     //Check column: O(1)
     for(int i=0; i<board.length; i++) {
       if(board[i][col] == entry) {
-        //System.out.println("COLUMN CHECK for entry: row = " + i + ", col = " + col + ", entry = " + board[row][col]);
         return false;
       }
     }
@@ -177,7 +180,6 @@ public class GUI_Solver extends JFrame implements ActionListener {
     for(int num=1; num<=board.length; num++) {
       if(isValidEntry(board, row, col, num)) {
         board[row][col] = num;
-        //printBoard(board);
         if(solveBoard(board)) {
           boxes[row][col].setEditable(false);
           return true;
@@ -185,9 +187,6 @@ public class GUI_Solver extends JFrame implements ActionListener {
         else {
           board[row][col] = 0;
         }
-      }
-      else {
-        boxes[row][col].setBackground(Color.RED);
       }
     }
 
@@ -246,5 +245,17 @@ public class GUI_Solver extends JFrame implements ActionListener {
 
   public static void checkUserEntries(int[][] board) {
 		//Check user entries based on final answer
+    for(int i=0; i<board.length; i++) {
+      for(int j=0; j<board.length; j++) {
+        if(board[i][j] != 0) {
+          if(isValidEntry(board, i, j, board[i][j])) {
+            boxes[i][j].setBackground(Color.BLUE);
+          }
+          else {
+            boxes[i][j].setBackground(Color.RED);
+          }
+        }
+      }
+    }
   }
 }
